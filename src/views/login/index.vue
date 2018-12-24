b56635743c6d4fbf1de995b1541294b1e7165372
qiji <17777788217@163.com>
2018-12-24 18:02:57 +0800

    second commit

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-    <h1 style="color: #eee; text-align: center;">炼石CipherGateway | 密钥生命周期管理系统</h1>
+    <h1 style="color: #eee; text-align: center;">数据加密管理系统</h1>
     <el-form
       :model="loginForm"
       :rules="loginRules"
